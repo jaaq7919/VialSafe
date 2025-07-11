fddd333061a0e8abd78d28c34b7a281c8cc91316
joan alexis acosta quiroz <jaaq7919@gmail.com>
2025-07-11 14:18:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/client/heatmap.tsx
+++ src/components/client/heatmap.tsx
@@ -2,7 +2,8 @@
 
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
+import L from 'leaflet';
 
 // leaflet.heat depends on the global L object, so we need to handle it carefully
 // We will import it dynamically inside a useEffect hook.
@@ -15,8 +16,7 @@ const HeatmapLayer: React.FC<HeatmapLayerProps> = ({ data }) => {
   const map = useMap();
 
   useEffect(() => {
-    // Dynamically import leaflet and leaflet.heat only on the client side
-    const L = require('leaflet');
+    // Dynamically import leaflet.heat only on the client side
     require('leaflet.heat');
 
     // Clear existing heat layers to avoid duplication on re-render
@@ -48,15 +48,20 @@ const Heatmap: React.FC<HeatmapProps> = ({ data }) => {
   // Default coordinates for Florida, Valle del Cauca
   const position: [number, number] = [3.423, -76.324];
 
-  return (
-    <MapContainer center={position} zoom={14} style={{ height: '100%', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <HeatmapLayer data={data} />
-    </MapContainer>
+  const displayMap = useMemo(
+    () => (
+      <MapContainer center={position} zoom={14} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <HeatmapLayer data={data} />
+      </MapContainer>
+    ),
+    [data]
   );
+
+  return <>{displayMap}</>;
 };
 
 export default Heatmap;
